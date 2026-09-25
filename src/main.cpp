6d0447051830c060c103710c45f2b43376503ef3
HoJLter <hojlter.work@gmail.com>
2026-09-25 03:42:23 +0300

    Add: SFML initial state

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include "SFML/Graphics.hpp"
+
+int main()
+{
+    sf::RenderWindow window(
+        sf::VideoMode({ 800, 800 }),
+        "SFML"
+    );
+
+    while (window.isOpen())
+    {
+        while (auto event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
+}
